d092fd5bb43e8c97fbc45bda1f6e172bf2a1d13b
frankfang <frankfang1990@gmail.com>
2022-04-18 22:47:20 +0800

    使用 replace 代替 push 避免后退

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -16,13 +16,13 @@ export const Welcome = defineComponent({
     const { direction, swiping } = useSwipe(main, { beforeStart: e => e.preventDefault() })
     const route = useRoute()
     const router = useRouter()
-    const push = throttle(() => {
+    const replace = throttle(() => {
       const name = (route.name || 'Welcome1').toString()
-      router.push(pushMap[name])
+      router.replace(pushMap[name])
     }, 500)
     watchEffect(() => {
       if (swiping.value && direction.value === 'left') {
-        push()
+        replace()
       }
     })
     return () => <div class={s.wrapper}>
