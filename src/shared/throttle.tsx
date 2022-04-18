3d50747b801c8fbefa77897988f74e6a403d1d9b
frankfang <frankfang1990@gmail.com>
2022-04-18 22:59:33 +0800

    做了一下 TS 体操，锻炼了一下大脑

--- src/shared/throttle.tsx
+++ src/shared/throttle.tsx
@@ -1,13 +1,15 @@
-export const throttle = (fn: Function, time: number) => {
+export const throttle = <T extends ((...args: unknown[]) => any)>(fn: T, time: number) => {
   let timer: number | undefined = undefined
-  return (...args: any[]) => {
+  let result: ReturnType<T>
+  return (...args: Parameters<T>) => {
     if (timer) {
-      return
+      return result
     } else {
-      fn(...args)
+      result = fn(...args)
       timer = setTimeout(() => {
         timer = undefined
       }, time)
+      return result
     }
   }
 }
