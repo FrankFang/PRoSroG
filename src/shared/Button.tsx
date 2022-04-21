e57b11b87a3851a527680774e89e9ea790506dd3
frankfang <frankfang1990@gmail.com>
2022-04-21 23:02:22 +0800

    onClick 可选

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -2,7 +2,7 @@ import { defineComponent } from 'vue';
 import s from './Button.module.scss';
 
 interface Props{
-  onClick: (e: MouseEvent) => void
+  onClick?: (e: MouseEvent) => void
 }
 
 export const Button = defineComponent<Props>({
