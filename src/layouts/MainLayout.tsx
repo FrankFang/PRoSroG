4c1ab8db25fd12bb2029f777eb07ce18a2c399f7
frankfang <frankfang1990@gmail.com>
2022-04-27 22:56:46 +0800

    创建 MainLayout 布局

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { defineComponent, PropType } from 'vue';
+import { Navbar } from '../shared/Navbar';
+export const MainLayout = defineComponent({
+  setup: (props, context) => {
+    return () => (
+      <div>
+      <Navbar>{
+        {
+          default: () => context.slots.title?.(),
+          icon: () => context.slots.icon?.(),
+        }
+      }</Navbar>
+      {context.slots.default?.()}
+      </div>
+    )
+  }
+})
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, ref } from 'vue';
 import { RouterLink } from 'vue-router';
+import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
 import { Center } from '../shared/Center';
 import { FloatButton } from '../shared/FloatButton';
@@ -14,28 +15,28 @@ export const StartPage = defineComponent({
       refOverlayVisible.value = !refOverlayVisible.value
     }
     return () => (
-      <div>
-        <Navbar>{
-          {
-            default: () => '山竹记账',
-            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
-          }
-        }</Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name="pig" class={s.pig} />
-        </Center>
-        <div class={s.button_wrapper}>
-          <RouterLink to="/items/create">
-            <Button class={s.button}>开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/items/create">
-          <FloatButton iconName='add' />
-        </RouterLink>
-        {refOverlayVisible.value &&
-          <Overlay onClose={() => refOverlayVisible.value = false} />
+      <MainLayout>{
+        {
+          title: () => '山竹记账',
+          icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />,
+          default: () => <>
+            <Center class={s.pig_wrapper}>
+              <Icon name="pig" class={s.pig} />
+            </Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to="/items/create">
+                <Button class={s.button}>开始记账</Button>
+              </RouterLink>
+            </div>
+            <RouterLink to="/items/create">
+              <FloatButton iconName='add' />
+            </RouterLink>
+            {refOverlayVisible.value &&
+              <Overlay onClose={() => refOverlayVisible.value = false} />
+            }
+          </>
         }
-      </div>
+      }</MainLayout>
     )
   }
 })
