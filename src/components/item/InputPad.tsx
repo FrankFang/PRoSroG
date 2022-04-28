d24fc7a7bc23b7c5c3a3f175726a48cff0cd5bad
frankfang <frankfang1990@gmail.com>
2022-04-28 23:05:54 +0800

    打算使用 grid

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -2,7 +2,7 @@ import { defineComponent, PropType, ref } from 'vue';
 import { Icon } from '../../shared/Icon';
 import { time } from '../../shared/time';
 import s from './InputPad.module.scss';
-import { DatetimePicker, Popup } from 'vant';
+import { DatetimePicker, NumberKeyboard, Popup } from 'vant';
 export const InputPad = defineComponent({
   props: {
     name: {
@@ -12,28 +12,29 @@ export const InputPad = defineComponent({
   setup: (props, context) => {
     const now = new Date()
     const refDate = ref<Date>(now)
+    const appendText = (n: number | string) => refAmount.value += n.toString()
     const buttons = [
-      { text: '1', onClick: () => { } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
+      {
+        text: '1', onClick: () => { appendText(1) }
+      },
+      { text: '2', onClick: () => { appendText(2) } },
+      { text: '3', onClick: () => { appendText(3) } },
+      { text: '4', onClick: () => { appendText(4) } },
+      { text: '5', onClick: () => { appendText(5) } },
+      { text: '6', onClick: () => { appendText(6) } },
+      { text: '7', onClick: () => { appendText(7) } },
+      { text: '8', onClick: () => { appendText(8) } },
+      { text: '9', onClick: () => { appendText(9) } },
+      { text: '.', onClick: () => { appendText('.') } },
+      { text: '0', onClick: () => { appendText(0) } },
       { text: '清空', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '+', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '-', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
-      { text: '删', onClick: () => { } },
       { text: '提交', onClick: () => { } },
     ]
     const refDatePickerVisible = ref(false)
     const showDatePicker = () => refDatePickerVisible.value = true
     const hideDatePicker = () => refDatePickerVisible.value = false
     const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
+    const refAmount = ref('')
     return () => <>
       <div class={s.dateAndAmount}>
         <span class={s.date}>
@@ -47,7 +48,7 @@ export const InputPad = defineComponent({
             </Popup>
           </span>
         </span>
-        <span class={s.amount}>199.12</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
       <div class={s.buttons}>
         {buttons.map(button =>
