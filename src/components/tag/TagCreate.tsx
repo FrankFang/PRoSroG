968a98d6483cd224f5b73a0ea976ca0b946fd01a
frankfang <frankfang1990@gmail.com>
2022-05-05 23:01:00 +0800

    完成 EmojiSelect 组件

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -13,7 +13,7 @@ export const TagCreate = defineComponent({
   setup: (props, context) => {
     const formData = reactive({
       name: '',
-      sign: '',
+      sign: 'x',
     })
     return () => (
       <MainLayout>{{
@@ -34,9 +34,9 @@ export const TagCreate = defineComponent({
             </div>
             <div class={s.formRow}>
               <label class={s.formLabel}>
-                <span class={s.formItem_name}>符号</span>
+                <span class={s.formItem_name}>符号 {formData.sign}</span>
                 <div class={s.formItem_value}>
-                  <EmojiSelect class={[s.formItem, s.emojiList, s.error]} />
+                  <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]} />
                 </div>
                 <div class={s.formItem_errorHint}>
                   <span>必填</span>
--- src/shared/EmojiSelect.tsx
+++ src/shared/EmojiSelect.tsx
@@ -1,14 +1,13 @@
-import { defineComponent, PropType, ref } from 'vue';
+import { computed, defineComponent, PropType, ref } from 'vue';
 import { emojiList } from './emojiList';
 import s from './EmojiSelect.module.scss';
 export const EmojiSelect = defineComponent({
   props: {
-    name: {
-      type: String as PropType<string>
+    modelValue: {
+      type: String
     }
   },
   setup: (props, context) => {
-    console.log(emojiList)
     const refSelected = ref(1)
     const table: [string, string[]][] = [
       ['表情', ['face-smiling', 'face-affection', 'face-tongue', 'face-hand',
@@ -30,18 +29,29 @@ export const EmojiSelect = defineComponent({
       ]],
       ['运动', ['sport', 'game']],
     ]
-    const selectedItem = table[refSelected.value][1]
-    const emojis = selectedItem.map(category =>
-      emojiList.find(item => item[0] === category)?.[1]
-        .map(item => <li>{item}</li>)
-    )
+    const onClickTab = (index: number) => {
+      refSelected.value = index
+    }
+    const onClickEmoji = (emoji: string) => {
+      context.emit('update:modelValue', emoji)
+    }
+    const emojis = computed(() => {
+      const selectedItem = table[refSelected.value][1]
+      return selectedItem.map(category =>
+        emojiList.find(item => item[0] === category)?.[1]
+          .map(item => <li class={item === props.modelValue ? s.selectedEmoji : ''}
+            onClick={() => onClickEmoji(item)}>{item}</li>)
+      )
+    })
     return () => (
       <div class={s.emojiList}>
         <nav>
-          {table.map(item => <span>{item[0]}</span>)}
+          {table.map((item, index) =>
+            <span class={index === refSelected.value ? s.selected : ''}
+              onClick={() => onClickTab(index)}>{item[0]}</span>)}
         </nav>
         <ol>
-          {emojis}
+          {emojis.value}
         </ol>
       </div>
     )
