c5a491af399bf20267957fd7857de8b5b868f007
frankfang <frankfang1990@gmail.com>
2022-05-10 22:49:07 +0800

    完成记一笔的样式

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -12,21 +12,103 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, context) => {
     const refKind = ref('支出')
+    const refExpensesTags = ref([
+      { id: 1, name: '餐费', sign: '￥', category: 'expenses' },
+      { id: 2, name: '打车', sign: '￥', category: 'expenses' },
+      { id: 3, name: '聚餐', sign: '￥', category: 'expenses' },
+      { id: 4, name: '打车', sign: '￥', category: 'expenses' },
+      { id: 5, name: '聚餐', sign: '￥', category: 'expenses' },
+      { id: 6, name: '打车', sign: '￥', category: 'expenses' },
+      { id: 7, name: '聚餐', sign: '￥', category: 'expenses' },
+    ])
+    const refIncomeTags = ref([
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+    ])
     return () => (
-      <MainLayout>{{
+      <MainLayout class={s.layout}>{{
         title: () => '记一笔',
         icon: () => <Icon name="left" class={s.navIcon} />,
         default: () => <>
-          <Tabs v-model:selected={refKind.value}>
-            <Tab name="支出">
-              icon 列表
-            </Tab>
-            <Tab name="收入">
-              icon 列表2
-            </Tab>
-          </Tabs>
-          <div class={s.inputPad_wrapper}>
-            <InputPad />
+          <div class={s.wrapper}>
+            <Tabs v-model:selected={refKind.value} class={s.tabs}>
+              <Tab name="支出" class={s.tags_wrapper}>
+                <div class={s.tag}>
+                  <div class={s.sign}>
+                    <Icon name="add" class={s.createTag} />
+                  </div>
+                  <div class={s.name}>
+                    新增
+                  </div>
+                </div>
+                {refExpensesTags.value.map(tag =>
+                  <div class={[s.tag, s.selected]}>
+                    <div class={s.sign}>
+                      {tag.sign}
+                    </div>
+                    <div class={s.name}>
+                      {tag.name}
+                    </div>
+                  </div>
+                )}
+              </Tab>
+              <Tab name="收入" class={s.tags_wrapper}>
+                <div class={s.tag}>
+                  <div class={s.sign}>
+                    <Icon name="add" class={s.createTag} />
+                  </div>
+                  <div class={s.name}>
+                    新增
+                  </div>
+                </div>
+                {refIncomeTags.value.map(tag =>
+                  <div class={[s.tag, s.selected]}>
+                    <div class={s.sign}>
+                      {tag.sign}
+                    </div>
+                    <div class={s.name}>
+                      {tag.name}
+                    </div>
+                  </div>
+                )}
+              </Tab>
+            </Tabs>
+            <div class={s.inputPad_wrapper}>
+              <InputPad />
+            </div>
           </div>
         </>
       }}</MainLayout>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,17 @@
 import { defineComponent, PropType } from 'vue';
 import { Navbar } from '../shared/Navbar';
+import s from './MainLayout.module.scss';
 export const MainLayout = defineComponent({
   setup: (props, context) => {
     return () => (
       <div>
-      <Navbar>{
-        {
-          default: () => context.slots.title?.(),
-          icon: () => context.slots.icon?.(),
-        }
-      }</Navbar>
-      {context.slots.default?.()}
+        <Navbar>{
+          {
+            default: () => context.slots.title?.(),
+            icon: () => context.slots.icon?.(),
+          }
+        }</Navbar>
+        {context.slots.default?.()}
       </div>
     )
   }
