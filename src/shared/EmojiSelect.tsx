5c7e59040293fe5d97d9e01a4083eb590a7ecb0a
frankfang <frankfang1990@gmail.com>
2022-05-12 23:37:17 +0800

    让 EmojiSelect 支持 v-model 和 props 两种事件形式

--- src/shared/EmojiSelect.tsx
+++ src/shared/EmojiSelect.tsx
@@ -5,6 +5,9 @@ export const EmojiSelect = defineComponent({
   props: {
     modelValue: {
       type: String
+    },
+    onUpdateModelValue: {
+      type: Function as PropType<(emoji: string) => void>
     }
   },
   setup: (props, context) => {
@@ -33,7 +36,11 @@ export const EmojiSelect = defineComponent({
       refSelected.value = index
     }
     const onClickEmoji = (emoji: string) => {
-      context.emit('update:modelValue', emoji)
+      if (props.onUpdateModelValue) {
+        props.onUpdateModelValue(emoji)
+      } else {
+        context.emit('update:modelValue', emoji)
+      }
     }
     const emojis = computed(() => {
       const selectedItem = table[refSelected.value][1]
