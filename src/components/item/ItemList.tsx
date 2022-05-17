1e84da5c18b1b96a2e7d425a1d446f9032f48a3b
frankfang <frankfang1990@gmail.com>
2022-05-17 21:52:54 +0800

    每次点击自定义时间都弹出弹出框

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -30,11 +30,6 @@ export const ItemList = defineComponent({
         end: time.lastDayOfYear()
       }
     ]
-    watchEffect(() => {
-      if (refSelected.value === '自定义时间') {
-        refOverlayVisible.value = true
-      }
-    })
     const refOverlayVisible = ref(false)
     const onSubmitCustomTime = (e: Event) => {
       e.preventDefault()
@@ -46,7 +41,8 @@ export const ItemList = defineComponent({
           title: () => '山竹记账',
           icon: () => <Icon name="menu" />,
           default: () => <>
-            <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+            <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}
+              onUpdate:selected={() => refOverlayVisible.value = true}>
               <Tab name="本月">
                 <ItemSummary
                   startDate={timeList[0].start.format()}
--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -9,11 +9,8 @@ export const Tabs = defineComponent({
       type: String as PropType<string>,
       required: false,
     },
-    onUpdateSelected: {
-      type: Function as PropType<(name: string) => void>,
-      required: false,
-    }
   },
+  emits: ['update:selected'],
   setup: (props, context) => {
     return () => {
       const tabs = context.slots.default?.()
