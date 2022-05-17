d603cc9088153b3a525c2883ce23c11630bbf278
frankfang <frankfang1990@gmail.com>
2022-05-17 23:40:39 +0800

    创建 Charts 组件

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,5 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
+import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
 export const Charts = defineComponent({
   props: {
@@ -12,8 +13,14 @@ export const Charts = defineComponent({
     }
   },
   setup: (props, context) => {
+    const category = ref('expenses')
     return () => (
-      <div class={s.wrapper}>图表</div>
+      <div class={s.wrapper}>
+        <FormItem label='类型' type="select" options={[
+          { value: 'expenses', text: '支出' },
+          { value: 'income', text: '收入' }
+        ]} v-model={category.value} />
+      </div>
     )
   }
 })
--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -28,12 +28,13 @@ export const FormItem = defineComponent({
       type: [String, Number]
     },
     type: {
-      type: String as PropType<'text' | 'emojiSelect' | 'date' | 'validationCode'>,
+      type: String as PropType<'text' | 'emojiSelect' | 'date' | 'validationCode' | 'select'>,
     },
     error: {
       type: String
     },
     placeholder: String,
+    options: Array as PropType<Array<{ value: string, text: string }>>
   },
   emits: ['update:modelValue'],
   setup: (props, context) => {
@@ -59,6 +60,13 @@ export const FormItem = defineComponent({
               发送验证码
             </Button>
           </>
+        case 'select':
+          return <select class={[s.formItem, s.select]} value={props.modelValue}
+            onChange={(e: any) => { context.emit('update:modelValue', e.target.value) }}>
+            {props.options?.map(option =>
+              <option value={option.value}>{option.text}</option>
+            )}
+          </select>
         case 'date':
           return <>
             <input readonly={true} value={props.modelValue}
