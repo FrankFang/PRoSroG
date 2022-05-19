1991da0fbbd9911f2112e4e613288040f2ea0c59
frankfang <frankfang1990@gmail.com>
2022-05-19 21:59:41 +0800

    完成第三个图表

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { computed, defineComponent, onMounted, PropType, reactive, ref } from 'vue';
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
 
@@ -21,6 +21,18 @@ export const Charts = defineComponent({
     const category = ref('expenses')
     const refDiv = ref<HTMLDivElement>()
     const refDiv2 = ref<HTMLDivElement>()
+    const data3 = reactive([
+      { tag: { id: 1, name: '房租', sign: 'x' }, amount: 3000 },
+      { tag: { id: 2, name: '吃饭', sign: 'x' }, amount: 1000 },
+      { tag: { id: 3, name: '娱乐', sign: 'x' }, amount: 900 },
+    ])
+    const betterData3 = computed(() => {
+      const total = data3.reduce((sum, item) => sum + item.amount, 0)
+      return data3.map(item => ({
+        ...item,
+        percent: Math.round(item.amount / total * 100) + '%'
+      }))
+    })
     onMounted(() => {
       if (refDiv.value === undefined) { return }
       // 基于准备好的dom，初始化echarts实例
@@ -87,6 +99,26 @@ export const Charts = defineComponent({
         ]} v-model={category.value} />
         <div ref={refDiv} class={s.demo}></div>
         <div ref={refDiv2} class={s.demo2}></div>
+        <div class={s.demo3}>
+          {betterData3.value.map(({ tag, amount, percent }) => {
+            return (
+              <div class={s.topItem}>
+                <div class={s.sign}>
+                  {tag.sign}
+                </div>
+                <div class={s.bar_wrapper}>
+                  <div class={s.bar_text}>
+                    <span> {tag.name} - {percent} </span>
+                    <span> ￥{amount} </span>
+                  </div>
+                  <div class={s.bar}>
+                    <div class={s.bar_inner}></div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
