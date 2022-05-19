053e89fb6c119b255871d361dfe61f951965d999
frankfang <frankfang1990@gmail.com>
2022-05-19 12:38:09 +0800

    引入 echarts

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,6 +1,11 @@
-import { defineComponent, PropType, ref } from 'vue';
+import { defineComponent, onMounted, PropType, ref } from 'vue';
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
+
+
+import * as echarts from 'echarts';
+
+
 export const Charts = defineComponent({
   props: {
     startDate: {
@@ -14,12 +19,74 @@ export const Charts = defineComponent({
   },
   setup: (props, context) => {
     const category = ref('expenses')
+    const refDiv = ref<HTMLDivElement>()
+    const refDiv2 = ref<HTMLDivElement>()
+    onMounted(() => {
+      if (refDiv.value === undefined) { return }
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(refDiv.value);
+      // 绘制图表
+      myChart.setOption({
+        grid: [
+          { left: 0, top: 0, right: 0, bottom: 20 }
+        ],
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line'
+          }
+        ]
+      });
+
+    })
+    onMounted(() => {
+      if (refDiv2.value === undefined) { return }
+      // 基于准备好的dom，初始化echarts实例
+      var myChart = echarts.init(refDiv2.value);
+      // 绘制图表
+      const option = {
+        grid: [
+          { left: 0, top: 0, right: 0, bottom: 20 }
+        ],
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: '50%',
+            data: [
+              { value: 1048, name: 'Search Engine' },
+              { value: 735, name: 'Direct' },
+              { value: 580, name: 'Email' },
+              { value: 484, name: 'Union Ads' },
+              { value: 300, name: 'Video Ads' }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      };
+      myChart.setOption(option);
+    })
     return () => (
       <div class={s.wrapper}>
         <FormItem label='类型' type="select" options={[
           { value: 'expenses', text: '支出' },
           { value: 'income', text: '收入' }
         ]} v-model={category.value} />
+        <div ref={refDiv} class={s.demo}></div>
+        <div ref={refDiv2} class={s.demo2}></div>
       </div>
     )
   }
