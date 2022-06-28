cb563d0630d92b8fa96eeda30bc01f898bca23c7
frankfang <frankfang1990@gmail.com>
2022-06-29 00:25:20 +0800

    优化了，但是代码行数变多了

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,33 @@ import { App } from './App'
 import { createRouter } from 'vue-router'
 import { history } from './shared/history';
 import '@svgstore';
-import { http } from './shared/Http';
 import { fetchMe, mePromise } from './shared/me';
 
 const router = createRouter({ history, routes })
 
 fetchMe()
 
-router.beforeEach(async (to, from) => {
-  if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in')
-    || to.path === '/start') {
-    return true
-  } else {
-    const path = await mePromise!.then(
-      () => true,
-      () => '/sign_in?return_to=' + to.path
-    )
-    return path
+const whiteList: Record<string, 'exact' | 'startsWith'> = {
+  '/': 'exact',
+  '/start': 'exact',
+  '/welcome': 'startsWith',
+  '/sign_in': 'startsWith',
+}
+
+router.beforeEach((to, from) => {
+  for (const key in whiteList) {
+    const value = whiteList[key]
+    if (value === 'exact' && to.path === key) {
+      return true
+    }
+    if (value === 'startsWith' && to.path.startsWith(key)) {
+      return true
+    }
   }
+  return mePromise!.then(
+    () => true,
+    () => '/sign_in?return_to=' + to.path
+  )
 })
 
 const app = createApp(App)
