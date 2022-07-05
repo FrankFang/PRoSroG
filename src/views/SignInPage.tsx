e609f1b46fcfbe1dd2cc129bd2190c38ccf87495
frankfang <frankfang1990@gmail.com>
2022-07-05 22:31:27 +0800

    删除测试代码

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -36,9 +36,8 @@ export const SignInPage = defineComponent({
         { key: 'code', type: 'required', message: '必填' },
       ]))
       if (!hasError(errors)) {
-        const response = await http.post<{ jwt: string }>('/session', formData, {
-          params: { _mock: 'session' }
-        }).catch(onError)
+        const response = await http.post<{ jwt: string }>('/session', formData)
+          .catch(onError)
         console.log(response)
         localStorage.setItem('jwt', response.data.jwt)
         // router.push('/sign_in?return_to='+ encodeURIComponent(route.fullPath))
