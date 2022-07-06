773fd1ba6251fd590e0c3e80b0b503484ff339a7
frankfang <frankfang1990@gmail.com>
2022-07-06 23:16:17 +0800

    安装 prettier

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,48 +1,49 @@
-/// <reference types=vite/client />
+/// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
 
-declare module '*.scss' {
-  const content: Record<string, any> = {}
-  export default content
-}
-
-type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
+type JSONValue =
+  | null
+  | boolean
+  | string
+  | number
+  | JSONValue[]
+  | Record<string, JSONValue>;
 
 type Tag = {
-  id: number,
-  user_id: number,
-  name: string,
-  sign: string,
-  kind: expenses | income
-}
+  id: number;
+  user_id: number;
+  name: string;
+  sign: string;
+  kind: expenses | income;
+};
 type Item = {
-  id: number
-  user_id: number
-  amount: number
-  tags_id: number[]
-  happen_at: string
-  kind: expenses | income
-}
+  id: number;
+  user_id: number;
+  amount: number;
+  tags_id: number[];
+  happen_at: string;
+  kind: expenses | income;
+};
 
 type Resources<T = any> = {
-  resources: T[]
+  resources: T[];
   pager: {
-    page: number,
-    per_page: number,
-    count: number
-  }
-}
+    page: number;
+    per_page: number;
+    count: number;
+  };
+};
 
 type Resource<T> = {
-  resource: T
-}
+  resource: T;
+};
 
 type ResourceError = {
-  errors: Record<string, string[]>
-}
+  errors: Record<string, string[]>;
+};
