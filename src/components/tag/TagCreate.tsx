3d36631b4b6d95f49ef35cf5c841b2f4f799af34
frankfang <frankfang1990@gmail.com>
2022-07-06 23:41:14 +0800

    格式化

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -12,29 +12,34 @@ export const TagCreate = defineComponent({
     const formData = reactive({
       name: '',
       sign: '',
-    })
-    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
+    });
+    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
     const onSubmit = (e: Event) => {
       const rules: Rules<typeof formData> = [
         { key: 'name', type: 'required', message: '必填' },
-        { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
+        {
+          key: 'name',
+          type: 'pattern',
+          regex: /^.{1,4}$/,
+          message: '只能填 1 到 4 个字符',
+        },
         { key: 'sign', type: 'required', message: '必填' },
-      ]
+      ];
       Object.assign(errors, {
         name: undefined,
-        sign: undefined
-      })
-      Object.assign(errors, validate(formData, rules))
-      e.preventDefault()
-    }
+        sign: undefined,
+      });
+      Object.assign(errors, validate(formData, rules));
+      e.preventDefault();
+    };
     return () => (
-      <MainLayout>{{
-        title: () => '新建标签',
-        icon: () => <BackIcon />,
-        default: () => (
-          <TagForm />
-        )
-      }}</MainLayout>
-    )
-  }
-})
+      <MainLayout>
+        {{
+          title: () => '新建标签',
+          icon: () => <BackIcon />,
+          default: () => <TagForm />,
+        }}
+      </MainLayout>
+    );
+  },
+});
