feda9f1f1aacb909dea4c0f0b9a6d5a956f4002a
frankfang <frankfang1990@gmail.com>
2022-07-08 00:10:10 +0800

    格式化

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,47 +1,63 @@
-import { Dialog } from 'vant';
-import { defineComponent } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { MainLayout } from '../../layouts/MainLayout';
-import { BackIcon } from '../../shared/BackIcon';
-import { Button } from '../../shared/Button';
-import { http } from '../../shared/Http';
-import s from './Tag.module.scss';
-import { TagForm } from './TagForm';
+import { Dialog } from 'vant'
+import { defineComponent } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { MainLayout } from '../../layouts/MainLayout'
+import { BackIcon } from '../../shared/BackIcon'
+import { Button } from '../../shared/Button'
+import { http } from '../../shared/Http'
+import s from './Tag.module.scss'
+import { TagForm } from './TagForm'
 export const TagEdit = defineComponent({
   setup: (props, context) => {
     const route = useRoute()
     const numberId = parseInt(route.params.id!.toString())
-    if(Number.isNaN(numberId)){
-      return ()=> <div>id 不存在</div>
+    if (Number.isNaN(numberId)) {
+      return () => <div>id 不存在</div>
     }
     const router = useRouter()
-    const onError = ()=>{
-      Dialog.alert({ title:'提示',message:'删除失败' })
+    const onError = () => {
+      Dialog.alert({ title: '提示', message: '删除失败' })
     }
-    const onDelete = async (options?: {withItems?: boolean})=>{
+    const onDelete = async (options?: { withItems?: boolean }) => {
       await Dialog.confirm({
-        title:'确认',
-        message:'你真的要删除吗？'
+        title: '确认',
+        message: '你真的要删除吗？',
       })
-      await http.delete(`/tags/${numberId}`, {
-        withItems: options?.withItems ? 'true' : 'false'
-      }).catch(onError)
+      await http
+        .delete(`/tags/${numberId}`, {
+          withItems: options?.withItems ? 'true' : 'false',
+        })
+        .catch(onError)
       router.back()
     }
     return () => (
-      <MainLayout>{{
-        title: () => '编辑标签',
-        icon: () => <BackIcon />,
-        default: () => <>
-          <TagForm id={numberId} />
-          <div class={s.actions}>
-            <Button level='danger' class={s.removeTags}
-              onClick={()=>onDelete()}>删除标签</Button>
-            <Button level='danger' class={s.removeTagsAndItems}
-              onClick={()=>onDelete({withItems: true})}>删除标签和记账</Button>
-          </div>
-        </>
-      }}</MainLayout>
+      <MainLayout>
+        {{
+          title: () => '编辑标签',
+          icon: () => <BackIcon />,
+          default: () => (
+            <>
+              <TagForm id={numberId} />
+              <div class={s.actions}>
+                <Button
+                  level="danger"
+                  class={s.removeTags}
+                  onClick={() => onDelete()}
+                >
+                  删除标签
+                </Button>
+                <Button
+                  level="danger"
+                  class={s.removeTagsAndItems}
+                  onClick={() => onDelete({ withItems: true })}
+                >
+                  删除标签和记账
+                </Button>
+              </div>
+            </>
+          ),
+        }}
+      </MainLayout>
     )
-  }
+  },
 })
