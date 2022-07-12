b1242aceff281c29c324d5b9536dcb71443531f7
frankfang <frankfang1990@gmail.com>
2022-07-12 23:10:52 +0800

    格式化

--- src/components/statistics/LineChart.tsx
+++ src/components/statistics/LineChart.tsx
@@ -1,8 +1,15 @@
-import { defineComponent, onMounted, onUpdated, PropType, ref, watch } from 'vue';
-import s from './LineChart.module.scss';
-import * as echarts from 'echarts';
-import { Time } from '../../shared/time';
-import { getMoney } from '../../shared/Money';
+import {
+  defineComponent,
+  onMounted,
+  onUpdated,
+  PropType,
+  ref,
+  watch,
+} from 'vue'
+import s from './LineChart.module.scss'
+import * as echarts from 'echarts'
+import { Time } from '../../shared/time'
+import { getMoney } from '../../shared/Money'
 
 const echartsOption = {
   tooltip: {
@@ -10,7 +17,9 @@ const echartsOption = {
     trigger: 'axis',
     formatter: ([item]: any) => {
       const [x, y] = item.data
-      return `${new Time(new Date(x)).format('YYYY年MM月DD日')} ￥${getMoney(y)}`
+      return `${new Time(new Date(x)).format('YYYY年MM月DD日')} ￥${getMoney(
+        y
+      )}`
     },
   },
   grid: [{ left: 16, top: 20, right: 16, bottom: 20 }],
@@ -44,34 +53,41 @@ export const LineChart = defineComponent({
     data: {
       type: Array as PropType<[string, number][]>,
       required: true,
-    }
+    },
   },
   setup: (props, context) => {
     const refDiv = ref<HTMLDivElement>()
     let chart: echarts.ECharts | undefined = undefined
 
     onMounted(() => {
-      if (refDiv.value === undefined) { return }
+      if (refDiv.value === undefined) {
+        return
+      }
       // 基于准备好的dom，初始化echarts实例
-      chart = echarts.init(refDiv.value);
+      chart = echarts.init(refDiv.value)
       // 绘制图表
       chart.setOption({
         ...echartsOption,
-        series: [{
-          data: props.data,
-          type: 'line'
-        }]
-      });
+        series: [
+          {
+            data: props.data,
+            type: 'line',
+          },
+        ],
+      })
     })
-    watch(()=>props.data, ()=>{
-      chart?.setOption({
-        series: [{
-          data: props.data
-        }]
-      });
-    })
-    return () => (
-      <div ref={refDiv} class={s.wrapper}></div>
+    watch(
+      () => props.data,
+      () => {
+        chart?.setOption({
+          series: [
+            {
+              data: props.data,
+            },
+          ],
+        })
+      }
     )
-  }
+    return () => <div ref={refDiv} class={s.wrapper}></div>
+  },
 })
