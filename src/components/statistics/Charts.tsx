9ba51d73d14f809b3ffd47b02fc3e221e1be6b5c
frankfang <frankfang1990@gmail.com>
2022-07-13 22:36:18 +0800

    切换 kind 时重新请求数据

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,4 @@
-import { computed, defineComponent, onMounted, PropType, ref } from 'vue'
+import { computed, defineComponent, onMounted, PropType, ref, watch } from 'vue'
 import { FormItem } from '../../shared/Form'
 import s from './Charts.module.scss'
 import { LineChart } from './LineChart'
@@ -41,7 +41,7 @@ export const Charts = defineComponent({
       })
     })
 
-    onMounted(async () => {
+    const fetchData1 = async () => {
       const response = await http.get<{ groups: Data1; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
@@ -50,7 +50,9 @@ export const Charts = defineComponent({
         _mock: 'itemSummary'
       })
       data1.value = response.data.groups
-    })
+    }
+    onMounted(fetchData1)
+    watch(() => kind.value, fetchData1)
 
     const data2 = ref<Data2>([])
     const betterData2 = computed<{ name: string; value: number }[]>(() =>
@@ -68,7 +70,7 @@ export const Charts = defineComponent({
       }))
     })
 
-    onMounted(async () => {
+    const fetchData2 = async ()=>{
       const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
         happen_after: props.startDate,
         happen_before: props.endDate,
@@ -77,7 +79,9 @@ export const Charts = defineComponent({
         _mock: 'itemSummary'
       })
       data2.value = response.data.groups
-    })
+    }
+    onMounted(fetchData2)
+    watch(() => kind.value, fetchData2)
 
     return () => (
       <div class={s.wrapper}>
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -6,7 +6,8 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 faker.setLocale('zh_CN')
 
 export const mockItemSummary: Mock = (config) => {
-  if (config.params.group_by === 'happen_at') {
+  const { group_by, kind } = config.params
+  if (group_by === 'happen_at' && kind === 'expenses') {
     return [
       200,
       {
@@ -18,7 +19,19 @@ export const mockItemSummary: Mock = (config) => {
         summary: 600
       }
     ]
-  } else {
+  } else if (group_by === 'happen_at' && kind === 'income') {
+    return [
+      200,
+      {
+        groups: [
+          { happen_at: '2022-07-08T00:00:00.000+0800', amount: 100 },
+          { happen_at: '2022-07-12T00:00:00.000+0800', amount: 300 },
+          { happen_at: '2022-07-19T00:00:00.000+0800', amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  } else if (group_by === 'tag_id' && kind === 'expenses') {
     return [
       200,
       {
@@ -28,6 +41,18 @@ export const mockItemSummary: Mock = (config) => {
           { tag_id: 3, tag: { id: 3, name: '购物', sign: faker.internet.emoji() }, amount: 200 }
         ],
         summary: 600
+      }
+    ]
+  } else {
+    return [
+      200,
+      {
+        groups: [
+          { tag_id: 1, tag: { id: 1, name: '交通', sign: faker.internet.emoji() }, amount: 400 },
+          { tag_id: 2, tag: { id: 2, name: '吃饭', sign: faker.internet.emoji() }, amount: 300 },
+          { tag_id: 3, tag: { id: 3, name: '购物', sign: faker.internet.emoji() }, amount: 200 }
+        ],
+        summary: 900
       }
     ]
   }
