0137afd228d11aa801d2412dd3b8b0815a4c03bc
frankfang <frankfang1990@gmail.com>
2022-07-17 23:00:29 +0800

    将 mock 中的 tags_id 改为 tag_ids

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -88,11 +88,11 @@ export const mockItemIndex: Mock = (config) => {
       id: createId(),
       user_id: createId(),
       amount: Math.floor(Math.random() * 10000),
-      tags_id: [createId()],
+      tag_ids: [createId()],
       tags: [createTag()],
       happen_at: faker.date.past().toISOString(),
       kind: config.params.kind
-    }))
+    } as Item))
   const createBody = (n = 1, attrs?: any) => ({
     resources: createItem(n),
     pager: createPaper(page),
@@ -141,12 +141,12 @@ export const mockItemCreate: Mock = (config) => {
         user_id: 1312,
         amount: 9900,
         note: null,
-        tags_id: [3508],
+        tag_ids: [3508],
         happen_at: '2020-10-29T16:00:00.000Z',
         created_at: '2022-07-03T15:35:56.301Z',
         updated_at: '2022-07-03T15:35:56.301Z',
         kind: 'expenses'
-      }
+      } as Item
     }
   ]
 }
