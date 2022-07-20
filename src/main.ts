17a9cc08ed3012f0a47f859fbd5c6d70a1980afe
frankfang <frankfang1990@gmail.com>
2022-07-20 23:17:30 +0800

    使用 storeToRefs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { App } from './App'
 import { createRouter } from 'vue-router'
 import { history } from './shared/history';
 import '@svgstore';
-import { createPinia } from 'pinia';
+import { createPinia, storeToRefs } from 'pinia';
 
 
 const router = createRouter({ history, routes })
@@ -16,6 +16,7 @@ app.use(pinia)
 app.mount('#app')
 
 const meStore = useMeStore()
+const {mePromise} = storeToRefs(meStore)
 meStore.fetchMe()
 
 
@@ -37,7 +38,7 @@ router.beforeEach((to, from) => {
       return true
     }
   }
-  return meStore.mePromise!.then(
+  return mePromise!.value!.then(
     () => true,
     () => '/sign_in?return_to=' + to.path
   )
