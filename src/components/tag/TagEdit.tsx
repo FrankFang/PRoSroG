4f112ad85971135a59a98bbed50b9eff0726b4e2
frankfang <frankfang1990@gmail.com>
2022-07-21 22:48:35 +0800

    砍需求：只有一种删除标签的方式

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -41,17 +41,10 @@ export const TagEdit = defineComponent({
               <div class={s.actions}>
                 <Button
                   level="danger"
-                  class={s.removeTags}
-                  onClick={() => onDelete()}
-                >
-                  删除标签
-                </Button>
-                <Button
-                  level="danger"
                   class={s.removeTagsAndItems}
                   onClick={() => onDelete({ withItems: true })}
                 >
-                  删除标签和记账
+                  删除标签（对应记账也会被删除）
                 </Button>
               </div>
             </>
