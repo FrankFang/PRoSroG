57aef2ceadd228508a5766a611b426049d12981e
frankfang <frankfang1990@gmail.com>
2022-07-22 22:52:07 +0800

    修改 base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { svgstore } from './src/vite_plugins/svgstore';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://mangosteen-test-3.oss-cn-hangzhou.aliyuncs.com',
   plugins: [
     vue(),
     vueJsx({
