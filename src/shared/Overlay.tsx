ada88f0f8efca3abd4e22e58e41536771631b988
frankfang <frankfang1990@gmail.com>
2022-07-26 22:30:17 +0800

    退出登录后刷新页面

--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -10,7 +10,7 @@ export const Overlay = defineComponent({
       type: Function as PropType<() => void>,
     },
   },
-  setup: (props, context) => {
+  setup: (props) => {
     const meStore = useMeStore()
     const close = () => {
       props.onClose?.()
@@ -27,6 +27,7 @@ export const Overlay = defineComponent({
         message: '你真的要退出登录吗？',
       })
       localStorage.removeItem('jwt')
+      window.location.reload()
     }
     return () => (
       <>
@@ -80,7 +81,7 @@ export const Overlay = defineComponent({
 })
 
 export const OverlayIcon = defineComponent({
-  setup: (props, context) => {
+  setup: () => {
     const refOverlayVisible = ref(false)
     const onClickMenu = () => {
       refOverlayVisible.value = !refOverlayVisible.value
