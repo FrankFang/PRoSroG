79469622032343090a4e3d01f9e9f8e4079fec2c
frankfang <frankfang1990@gmail.com>
2022-07-27 00:42:39 +0800

    格式化

--- vite.config.ts
+++ vite.config.ts
@@ -6,42 +6,44 @@ import styleImport, { VantResolve } from 'vite-plugin-style-import';
 import { svgstore } from './src/vite_plugins/svgstore';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: any) {
-          if (id.includes('echarts')) {
-            return 'echarts';
-          }
-          if (id.includes('mock') || id.includes('faker')) {
-            return 'mock';
-          }
-          if (id.includes('vant')) {
-            return 'vant';
-          }
-          if (id.includes('node_modules')) {
-            return 'vendor';
+export default defineConfig(() => {
+  return {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: any) {
+            if (id.includes('echarts')) {
+              return 'echarts';
+            }
+            if (id.includes('mock') || id.includes('faker')) {
+              return 'mock';
+            }
+            if (id.includes('vant')) {
+              return 'vant';
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
           }
         }
       }
-    }
-  },
-  plugins: [
-    vue(),
-    vueJsx({
-      transformOn: true,
-      mergeProps: true
-    }),
-    svgstore(),
-    styleImport({
-      resolves: [VantResolve()],
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'http://121.196.236.94:3000/',
+    },
+    plugins: [
+      vue(),
+      vueJsx({
+        transformOn: true,
+        mergeProps: true
+      }),
+      svgstore(),
+      styleImport({
+        resolves: [VantResolve()],
+      }),
+    ],
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: 'http://121.196.236.94:3000/',
+        }
       }
     }
   }
