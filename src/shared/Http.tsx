a602534ab6e3bf5a6fdf051ba60383f0dc1d9225
frankfang <frankfang1990@gmail.com>
2022-07-27 01:00:35 +0800

    删除没有必要的 dev 环境判断

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -27,14 +27,7 @@ export class Http {
   }
 }
 
-function isDev() {
-  if (location.hostname !== 'localhost' && location.hostname !== '127.0.0.1' && location.hostname !== '192.168.3.57') {
-    return false
-  }
-  return true
-}
-
-export const http = new Http(isDev() ? 'api/v1' : 'http://121.196.236.94:3000/api/v1')
+export const http = new Http(DEBUG ? 'api/v1' : 'http://121.196.236.94:3000/api/v1')
 
 http.instance.interceptors.request.use((config) => {
   const jwt = localStorage.getItem('jwt')
@@ -79,14 +72,6 @@ if (DEBUG) {
       mockTagShow
     }) => {
       const mock = (response: AxiosResponse) => {
-        if (
-          true ||
-          (location.hostname !== 'localhost' &&
-            location.hostname !== '127.0.0.1' &&
-            location.hostname !== '192.168.3.57')
-        ) {
-          return false
-        }
         switch (response.config?._mock) {
           case 'tagIndex':
             ;[response.status, response.data] = mockTagIndex(response.config)
